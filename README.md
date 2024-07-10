50d8914f9403d61d308676b8655a543bb18fce61
Benjamin Bowman <94076505+BenT-Bowman@users.noreply.github.com>
2024-07-10 16:27:19 -0400

    README changes and generate_data.py

--- README.md
+++ README.md
@@ -5,16 +5,51 @@
 * Recommended location: ./Dataset/Unprocessed
 
 ## Run Preprocessing Script
-Script only accepts .dat files. Split Control and Patient by adding a capital 'P'.
+Script only accepts .dat files. Seperate Control and Patient by adding a capital 'P' in the first character of the patient string.
 
-`python ./preprocessing.py --file_data_path <path to .dat files> --save_file_directory <Path to Dataset Folder> --seq_length <optional default=500> --skip <optional default=50>`
+Usage: The script requires four command-line arguments:
+
+* data_file_path: The path to the input file. The current data parsing capability is limited.
+* save_file_directory: The directory to save the output file.
+* skip: The number of lines to skip (default is 50).
+* seq_length: The sequence length (default is 500).
+Example Command:
+
+
+`python process_files.py --data_file_path path/to/input_file.npy --save_file_directory path/to/save_directory --skip 100 --seq_length 600`\
+This command will:
+
+Process the file located at path/to/input_file.npy. \
+Save the output to the directory path/to/save_directory. \
+Skip the first 100 lines of the input file. \
+Use a sequence length of 600.
 
 ## Run WGAN_Training
-Script exclusively loads .npy files. Data is of shape (samples, num_channels, seq_length)
-`python ./WGAN_Training.py --data_file_path <path to .dat files>`
 
+Usage: The script requires one command-line argument:
+
+* data_file_path: The path to the input file. The file should be of .npy type.
+Example Command:
+
+`python process_data_file.py --data_file_path path/to/input_file.npy` \
+This command will process the file located at path/to/input_file.npy.
+### Notes
+* Ensure the path provided in --data_file_path is valid and points to a .npy file.
 ## Generate Data using model
-Modify path variables in generate_data.ipynb. Run all.
+
+Usage: The script requires three command-line arguments:
+
+* model_paths: A comma-separated list of paths to the model files (with .pth extension).
+* save_locations: A comma-separated list of paths where the generated samples will be saved.
+* num_2_gen (optional): The number of samples to generate. Defaults to 2000 if not provided.
+Example Command:
+
+`python process_models.py --model_paths path/to/model1.pth,path/to/model2.pth --save_locations path/to/save1,path/to/save2 --num_2_gen 1000`
+This command will:
+
+Load the models from path/to/model1.pth and path/to/model2.pth. \
+Save the generated samples to path/to/save1 and path/to/save2. \
+Generate 1000 samples. 
 
 ## Validation
-Modify path variables in Validation.ipynb and Validation.ipynb and run all. (These processes my take upwards of 1-3 hours depending on the script ran.)
+Modify path variables in Validation.ipynb and MLProject.ipynb and run all. (These processes my take upwards of 1-3 hours depending on the script ran.)
--- Utility Scripts/generate_data.py
+++ Utility Scripts/generate_data.py
@@ -4,59 +4,72 @@ import numpy as np
 import shutil
 import torch.nn as nn
 import torch
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir))) # This is hellish, I am not sure why relative imports did not work
-from modules.DCGAN import Generator
-# Path to the folder you want to empty
-model_path1 = r'..\saved_models\test\generator_normal.pth'
-gen_data_save_location1 = r'..\Dataset\Generated\gen_normal.npy'
-real_data_path1 = r'..\Dataset\Real\normal.npy'
+import argparse
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+from modules import DCGAN
 
-model_path2 = r'..\saved_models\test\generator_patient.pth'
-gen_data_save_location2 = r'..\Dataset\Generated\gen_patient.npy'
-real_data_path2 = r'..\Dataset\Real\patient.npy'
+def argparse_helper():
+    parser = argparse.ArgumentParser(description='Process some files.')
+    parser.add_argument('--model_paths', type=str, required=True, help='Path to the model (.pth) files, comma seperated.')
+    parser.add_argument('--save_locations', type=str, required=True, help='Path to the model (.pth) files, comma seperated.')
+    parser.add_argument('--num_2_gen', type=int, default=2000, help='Number of samples to generate.')
 
-num_samples_to_generate = 2000
+    args = parser.parse_args() 
+    model_paths = args.model_paths.split(',') 
+    save_locations = args.save_locations.split(',') 
+    if len(model_paths) != len(save_locations):
+        raise ValueError("Expected the same number of model paths as save locations.")
 
-#Normal Subjects
-generator1 = torch.load(model_path1)
+    return model_paths, save_locations, args.num_2_gen
 
-# Assuming generator is your trained generator model
-# and latent_vector_size is the size of the noise input for the generator
-
-# Number of samples to generate
-
-# Generate random latent vectors
-noise1 = torch.randn(num_samples_to_generate, 100, device="cuda")
-
-# Generating new data
-generator1.eval()  # Switching the generator to evaluation mode
-
-with torch.no_grad():  # No need to track gradients
-    generated_data1 = generator1(noise1)
-    # Reshaping to 19x500 right after generation
-    generated_data1 = generated_data1.view(num_samples_to_generate, 19, -1)
-
-# Converting to numpy for saving and further processing
-generated_data1_np = generated_data1.cpu().numpy()
-if not os.path.exists(gen_data_save_location1):
-    os.makedirs(os.path.dirname(gen_data_save_location1), exist_ok=True)
-
-np.save(gen_data_save_location1, generated_data1_np)
+model_paths, save_locations, num_samples_to_generate = argparse_helper()
 
 
-#Patient Subjects
-noise2 = torch.randn(num_samples_to_generate, 100, device="cuda")
+for model_path, save_location in zip(model_paths,save_locations):
+    print(model_path)
 
-generator2 = torch.load(model_path2)
+    #Normal Subjects
+    generator1 = torch.load(model_path)
 
-with torch.no_grad():  # No need to track gradients
-    generated_data2 = generator2(noise2)
-    # Reshaping to 19x500 right after generation
-    generated_data2 = generated_data2.view(num_samples_to_generate, 19, -1)
+    # Assuming generator is your trained generator model
+    # and latent_vector_size is the size of the noise input for the generator
 
-# Converting to numpy for saving and further processing
-generated_data2_np = generated_data2.cpu().numpy()
-if not os.path.exists(gen_data_save_location2):
-    os.makedirs(os.path.dirname(gen_data_save_location2), exist_ok=True)
+    # Number of samples to generate
 
-np.save(gen_data_save_location2, generated_data2_np)
+    # Generate random latent vectors
+    noise = torch.randn(num_samples_to_generate, 100, device="cuda")
+
+    # Generating new data
+    generator1.eval()  # Switching the generator to evaluation mode
+
+    with torch.no_grad():  # No need to track gradients
+        generated_data1 = generator1(noise)
+        # Reshaping to 19x500 right after generation
+        generated_data1 = generated_data1.view(num_samples_to_generate, 19, -1)
+
+    # Converting to numpy for saving and further processing
+    generated_data1_np = generated_data1.cpu().numpy()
+    if not os.path.exists(save_location):
+        os.makedirs(os.path.dirname(save_location), exist_ok=True)
+
+    np.save(save_location, generated_data1_np)
+
+
+# #Patient Subjects
+# noise2 = torch.randn(num_samples_to_generate, 100, device="cuda")
+
+# generator2 = torch.load(model_path2)
+
+# with torch.no_grad():  # No need to track gradients
+#     generated_data2 = generator2(noise2)
+#     # Reshaping to 19x500 right after generation
+#     generated_data2 = generated_data2.view(num_samples_to_generate, 19, -1)
+
+# # Converting to numpy for saving and further processing
+# generated_data2_np = generated_data2.cpu().numpy()
+# if not os.path.exists(gen_data_save_location2):
+#     os.makedirs(os.path.dirname(gen_data_save_location2), exist_ok=True)
+
+# np.save(gen_data_save_location2, generated_data2_np)
